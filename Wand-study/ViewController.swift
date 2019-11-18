dc3469dfb6a7842677da55e4df6ee8433cc06259
Daniel Yang <yangafc@bc.edu>
2019-11-17 22:28:08 -0500

    Initial Commit

---
+++ Wand-study/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wand-study
+//
+//  Created by Daniel Yang on 11/17/19.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
